37c603e7eccbd170f482599951bf9c64907fe15e
PierreBorine <pierre.borine@protonmail.com>
2026-02-05 21:33:59 +0100

    refactor(packages): nest utility functions in let-in block

--- packages/libs/v1.nix
+++ packages/libs/v1.nix
@@ -3,35 +3,35 @@
   clib,
   lib,
 }: let
-  mkVSVersion = {
-    version,
-    hash,
-  }: let
-    v = clib.normalizeVersion version;
-    attrs = {
-      "v${v}" = builders.mkVintageStoryV1 {inherit version hash;};
-      "v${v}-net8" = builders.mkDotnet8 attrs."v${v}";
-    };
-  in
-    attrs;
+  importMinorVersion = f: let
+    mkVSVersion = {
+      version,
+      hash,
+    }: let
+      v = clib.normalizeVersion version;
+      attrs = {
+        "v${v}" = builders.mkVintageStoryV1 {inherit version hash;};
+        "v${v}-net8" = builders.mkDotnet8 attrs."v${v}";
+      };
+    in
+      attrs;
 
-  mkMinorVersion = versions: let
-    versions' = map mkVSVersion versions;
-    highestVersionSet = clib.highestVersion versions';
+    mkMinorVersion = versions: let
+      versions' = map mkVSVersion versions;
+      highestVersionSet = clib.highestVersion versions';
 
-    # "1.20.4" => "1-20"
-    majorMinor =
-      clib.normalizeVersion (lib.versions.majorMinor highestVersionSet.version);
-    highestVersion = clib.normalizeVersion highestVersionSet.version;
+      # "1.20.4" => "1-20"
+      majorMinor =
+        clib.normalizeVersion (lib.versions.majorMinor highestVersionSet.version);
+      highestVersion = clib.normalizeVersion highestVersionSet.version;
 
-    # {v-1-20 = <der>; v1-20-net8 = <der>;}
-    latestVersion = {
-      "v${majorMinor}" = highestVersionSet."v${highestVersion}";
-      "v${majorMinor}-net8" = highestVersionSet."v${highestVersion}-net8";
-    };
-  in
-    clib.recursiveMergeAttrsList ([latestVersion] ++ versions');
-
-  importMinorVersion = f: mkMinorVersion (import f);
+      # {v-1-20 = <der>; v1-20-net8 = <der>;}
+      latestVersion = {
+        "v${majorMinor}" = highestVersionSet."v${highestVersion}";
+        "v${majorMinor}-net8" = highestVersionSet."v${highestVersion}-net8";
+      };
+    in
+      clib.recursiveMergeAttrsList ([latestVersion] ++ versions');
+  in mkMinorVersion (import f);
 in
   importMinorVersion
--- packages/libs/v2.nix
+++ packages/libs/v2.nix
@@ -3,40 +3,40 @@
   clib,
   lib,
 }: let
-  mkVSVersion = {
-    version,
-    hash,
-  }: let
-    v = clib.normalizeVersion version;
-    attrs = {
-      "v${v}" = builders.mkVintageStoryV2 {inherit version hash;};
-      # TODO: remove when 1.22 hits unstable
-      "v${v}-net8" = builtins.warn ''
-        'v${v}-net8' is deprecated, please use 'v${v}' instead. As of 1.21, Vintage Story officially uses dotnet8.
-      '' attrs."v${v}";
-    };
-  in
-    attrs;
+  importMinorVersion = f: let
+    mkVSVersion = {
+      version,
+      hash,
+    }: let
+      v = clib.normalizeVersion version;
+      attrs = {
+        "v${v}" = builders.mkVintageStoryV2 {inherit version hash;};
+        # TODO: remove when 1.22 hits unstable
+        "v${v}-net8" = builtins.warn ''
+          'v${v}-net8' is deprecated, please use 'v${v}' instead. As of 1.21, Vintage Story officially uses dotnet8.
+        '' attrs."v${v}";
+      };
+    in
+      attrs;
 
-  mkMinorVersion = versions: let
-    versions' = map mkVSVersion versions;
-    highestVersionSet = clib.highestVersion versions';
+    mkMinorVersion = versions: let
+      versions' = map mkVSVersion versions;
+      highestVersionSet = clib.highestVersion versions';
 
-    # "1.20.4" => "1-20"
-    majorMinor =
-      clib.normalizeVersion (lib.versions.majorMinor highestVersionSet.version);
-    highestVersion = clib.normalizeVersion highestVersionSet.version;
+      # "1.20.4" => "1-20"
+      majorMinor =
+        clib.normalizeVersion (lib.versions.majorMinor highestVersionSet.version);
+      highestVersion = clib.normalizeVersion highestVersionSet.version;
 
-    # {v-1-20 = <der>;}
-    latestVersion = {
-      "v${majorMinor}" = highestVersionSet."v${highestVersion}";
-      "v${majorMinor}-net8" = builtins.warn ''
-        'v${majorMinor}-net8' is deprecated, please use 'v${majorMinor}' instead. As of 1.21, Vintage Story officially uses dotnet8.
-      '' latestVersion."v${majorMinor}";
-    };
-  in
-    clib.recursiveMergeAttrsList ([latestVersion] ++ versions');
-
-  importMinorVersion = f: mkMinorVersion (import f);
+      # {v-1-20 = <der>;}
+      latestVersion = {
+        "v${majorMinor}" = highestVersionSet."v${highestVersion}";
+        "v${majorMinor}-net8" = builtins.warn ''
+          'v${majorMinor}-net8' is deprecated, please use 'v${majorMinor}' instead. As of 1.21, Vintage Story officially uses dotnet8.
+        '' latestVersion."v${majorMinor}";
+      };
+    in
+      clib.recursiveMergeAttrsList ([latestVersion] ++ versions');
+  in mkMinorVersion (import f);
 in
   importMinorVersion
