021c90dcfe0c140d7b666634ee6fc5f4cc0ef390
PierreBorine <pierre.borine@protonmail.com>
2026-04-03 23:46:27 +0200

    vintagestory: 1.22.0-rc.7

--- packages/1-22.nix
+++ packages/1-22.nix
@@ -39,4 +39,8 @@
     version = "1.22.0-rc.6";
     hash = "sha256-ZAFtT9jQ7YKMjBUW8TBwNgpU/7PHsb2rU6VTWSqWh7Y=";
   }
+  {
+    version = "1.22.0-rc.7";
+    hash = "sha256-eeiNjOYPbFNNvo69NZBl7etjyk0nKm9iT4win+pZUPk=";
+  }
 ]
