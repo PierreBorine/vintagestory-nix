c0fdcbd70c4ee62d340ed9ed845eea6efe622014
PierreBorine <pierre.borine@protonmail.com>
2026-04-21 22:29:09 +0200

    refactor(packages): use builtin functions from `lib`, don't inherit them

--- packages/libs/default.nix
+++ packages/libs/default.nix
@@ -1,46 +1,32 @@
 {lib}: let
-  inherit
-    (builtins)
-    foldl'
-    replaceStrings
-    head
-    attrValues
-    filter
-    splitVersion
-    ;
-  inherit (lib.attrsets) recursiveUpdate;
-  inherit (lib.versions) majorMinor;
-  inherit (lib.strings) versionOlder;
-  inherit (lib.lists) intersectLists;
-
   recursiveMergeAttrsList =
-    foldl' (acc: attr: recursiveUpdate acc attr) {};
+    lib.foldl' (acc: attr: lib.recursiveUpdate acc attr) {};
 
   # "1.20.4" => "1-20-4"
-  normalizeVersion = replaceStrings ["."] ["-"];
+  normalizeVersion = lib.replaceStrings ["."] ["-"];
 
   # {version = "1.20.4";} => "1-20"
   majorMinorNormalized = package:
-    normalizeVersion (majorMinor package.version);
+    normalizeVersion (lib.versions.majorMinor package.version);
 
-  getVersion = set: (head (attrValues set)).version;
+  getVersion = set: (lib.head (lib.attrValues set)).version;
 
   isStable = version:
-    (intersectLists (splitVersion version) ["pre" "rc"]) == [];
-  filterUnstable = filter (el: isStable (getVersion el));
+    (lib.intersectLists (lib.splitVersion version) ["pre" "rc"]) == [];
+  filterUnstable = lib.filter (el: isStable (getVersion el));
 
   highestVersion = versions: let
     versions' = filterUnstable versions;
   in
-    foldl' (
+    lib.foldl' (
       maxSet: set: let
         maxSetVersion = getVersion maxSet;
         setVersion = getVersion set;
       in
-        if versionOlder setVersion maxSetVersion
+        if lib.versionOlder setVersion maxSetVersion
         then maxSet // {version = maxSetVersion;}
         else set // {version = setVersion;}
-    ) (head versions')
+    ) (lib.head versions')
     versions';
 
   mkPackageSet = packages: let
