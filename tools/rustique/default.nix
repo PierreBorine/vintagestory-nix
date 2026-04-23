4f798dd8073ac73f45b0ede01550c287d8b3c3f3
PierreBorine <pierre.borine@protonmail.com>
2026-04-23 22:20:58 +0200

    rustique: 0.5.12 -> 0.5.14

--- tools/rustique/default.nix
+++ tools/rustique/default.nix
@@ -5,21 +5,20 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "Rustique";
-  version = "0.5.12";
+  version = "0.5.14";
 
   src = fetchFromGitHub {
     owner = "Tekunogosu";
     repo = "Rustique";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-5zdmujHdsDCmj4lVJFmdxZXjmOk0EaKCAQ5UWiv9HP0=";
+    hash = "sha256-3zKOEI6l2wUznO6sB0QDpEBiwFciQ3kHpbC+DMw0CyM=";
   };
 
   # tries to use clang and /usr/bin/mold, let's just not do that, and
   # use the GNU toolchain from stdenv
   postPatch = "rm -vf .cargo/config.toml";
 
-  # somehow, cargoHash fails to compute
-  cargoLock.lockFile = "${finalAttrs.src}/Cargo.lock";
+  cargoHash = "sha256-ZXbpTV2pGUPwK9DTxiTLGC6M9I+KYxcr0109GkFFuKs=";
 
   # unstable rust feature path_add_extension
   env.RUSTC_BOOTSTRAP = 1;
