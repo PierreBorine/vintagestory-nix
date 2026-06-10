7f4a97b6e627b5ba4c0d7c063b40c45feb11a7b7
PierreBorine <pierre.borine@protonmail.com>
2026-06-10 19:02:30 +0200

    refactor(builders): pass path to derivation directly to `callPackage`

--- builders/default.nix
+++ builders/default.nix
@@ -2,11 +2,11 @@ pkgs: rec {
   # mkVintageStoryVX {version = "1.20.4"; hash = "sha256-Hgp...";}
   # => <Vintage Story derivation>
   # 1.18.8 -> 1.20.12
-  mkVintageStoryV1 = pkgs.callPackage (import ./mk-vintagestory-v1.nix);
+  mkVintageStoryV1 = pkgs.callPackage ./mk-vintagestory-v1.nix;
   # 1.21
-  mkVintageStoryV2 = pkgs.callPackage (import ./mk-vintagestory-v2.nix);
+  mkVintageStoryV2 = pkgs.callPackage ./mk-vintagestory-v2.nix;
   # 1.22 -> ...
-  mkVintageStoryV3 = pkgs.callPackage (import ./mk-vintagestory-v3.nix);
+  mkVintageStoryV3 = pkgs.callPackage ./mk-vintagestory-v3.nix;
 
   # Wrapper function that selects the correct derivation to use
   # based on the version.
